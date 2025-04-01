0ebd226f2fc8002f9374e5c52c0cbe818037a208
Jjoaquin04 <jjoaquinfuenteslopez@gmail.com>
2025-04-01 11:54:59 +0200

    Todos los controladores de los tipos de eventos añadidos

--- src/main/java/com/example/reticket/ReticketApplication.java
+++ src/main/java/com/example/reticket/ReticketApplication.java
@@ -9,5 +9,4 @@ public class ReticketApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReticketApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/reticket/controller/WebController.java
+++ src/main/java/com/example/reticket/controller/WebController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.example.reticket.db.Event;
 import com.example.reticket.service.EventService;
 
+
 @Controller
 public class WebController {
     
@@ -20,14 +21,40 @@ public class WebController {
     @GetMapping("/")
     public String mainPage(Model model) {
         List<Event> eventos = eventService.getAllEvents();
-        model.addAttribute("eventos",eventos);
+        model.addAttribute("mainEvents",eventos);
         return "mainPageTemplate";
+    }
+    @GetMapping("/events")
+    public String allEventsPage(Model model)    {
+        List<Event> eventos = eventService.getAllEvents();
+        model.addAttribute("events", eventos);
+        return "allEventsTemplate";
     }
     @GetMapping("/concerts")
     public String concertsPage(Model model) {
-        List<Event> concerEvents = eventService.getEventsByType(Event.EventType.CONCERT);
-        model.addAttribute("conciertos", concerEvents);
+        List<Event> concertsEvents = eventService.getEventsByType(Event.EventType.CONCERT);
+        model.addAttribute("concerts", concertsEvents);
         return "concertsTemplate";
     }
+    @GetMapping("/festivals")
+    public String festivalPage(Model model) {
+        List<Event> festivalEvents = eventService.getEventsByType(Event.EventType.FESTIVAL);
+        model.addAttribute("festival",festivalEvents);
+        return "festivalTemplate";
+    }
+    @GetMapping("/sports")
+    public String sportsPage(Model model) {
+        List<Event> sportsEvents = eventService.getEventsByType(Event.EventType.SPORTS);
+        model.addAttribute("sports", sportsEvents);
+        return "sportsTemplate";
+    }
+    @GetMapping("/theater")
+    public String theaterPage(Model model)  {
+        List<Event> theaterEvents = eventService.getEventsByType(Event.EventType.THEATER);
+        model.addAttribute("theaters", theaterEvents);
+        return "theaterTemplate";
+    }
+    
+    
     
 }
