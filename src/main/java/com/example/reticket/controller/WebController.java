4a8eca5671ac2f60b581b9894595a2f9b97555f9
Jjoaquin04 <jjoaquinfuenteslopez@gmail.com>
2025-04-01 11:34:29 +0200

    Nuevo controlador

--- src/main/java/com/example/reticket/controller/WebController.java
+++ src/main/java/com/example/reticket/controller/WebController.java
@@ -6,17 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import com.example.reticket.db.Event;
-import com.example.reticket.repository.EventRepository;
 import com.example.reticket.service.EventService;
-import com.example.reticket.service.TicketService;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
-
 
 @Controller
 public class WebController {
@@ -25,14 +18,16 @@ public class WebController {
     private EventService eventService;
 
     @GetMapping("/")
-    public String index()   {
-        return "redirect:/mainPage";
-    }
-    @GetMapping("/mainPage")
     public String mainPage(Model model) {
         List<Event> eventos = eventService.getAllEvents();
-        model.addAttribute("eventos", eventos);
+        model.addAttribute("eventos",eventos);
         return "mainPageTemplate";
+    }
+    @GetMapping("/concerts")
+    public String concertsPage(Model model) {
+        List<Event> concerEvents = eventService.getEventsByType(Event.EventType.CONCERT);
+        model.addAttribute("conciertos", concerEvents);
+        return "concertsTemplate";
     }
     
 }
