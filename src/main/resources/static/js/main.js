e832858385445222326695fcae102c7995942545
Alexx019 <ariassoutoalex@gmail.com>
2025-04-02 12:40:57 +0200

    Página events creada

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const dateElements = document.querySelectorAll(".date-event");
+    dateElements.forEach((element) => {
+        element.textContent = element.textContent.replace("T", " ");
+    });
+});
