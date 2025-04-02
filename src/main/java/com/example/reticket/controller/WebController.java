e6f47d5e3c6dbc51fe62f6b22301865df3b6f6c6
Jjoaquin04 <jjoaquinfuenteslopez@gmail.com>
2025-04-02 11:19:36 +0200

    Prueba sin RestController

--- src/main/java/com/example/reticket/controller/WebController.java
+++ src/main/java/com/example/reticket/controller/WebController.java
@@ -1,13 +1,11 @@
 package com.example.reticket.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
+import org.springframework.web.bind.annotation.RestController;  
 import com.example.reticket.db.Event;
 import com.example.reticket.service.EventService;
 
