aaa53c5211ff33e6fdd04e3f8994510a5ecc7f82
CodeInIA <virus.vbs777@gmail.com>
2025-04-13 15:58:17 +0200

    .env variables loaded at the main app

--- src/main/java/com/example/reticket/ReticketApplication.java
+++ src/main/java/com/example/reticket/ReticketApplication.java
@@ -2,11 +2,17 @@ package com.example.reticket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ReticketApplication {
         public static void main(String[] args) {
+                // Load environment variables from .env
+                Dotenv dotenv = Dotenv.configure().load();
+                System.setProperty("MYSQL_URL_JDBC", dotenv.get("MYSQL_URL_JDBC"));
+                System.setProperty("MYSQL_USER", dotenv.get("MYSQL_USER"));
+                System.setProperty("MYSQL_PASS", dotenv.get("MYSQL_PASS"));
+                
                 SpringApplication.run(ReticketApplication.class, args);
         }
 }
